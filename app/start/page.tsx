f6e422b23209a2dd7ada7451d1e315f8409025d7
volant97 <volant97@naver.com>
2024-09-25 16:07:52 +0900

    [UPDATE] 0.1.1 01 start 페이지 제작

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -1,3 +1,26 @@
+import WordInput from "@/components/start/WordInput";
+
 export default function Start() {
-  return <div>시작</div>;
+  return (
+    <div className="flex flex-col justify-center items-center gap-14 text-3xl">
+      <div className="flex flex-col items-center gap-3">
+        <p>
+          <span className="font-bold text-blue-500">마스터플랜</span>에 오신
+          것을 환영합니다!
+        </p>
+        <p>
+          최고의 <span className="font-bold text-yellow-500">마스터</span>가
+          되실 당신을 기다리고 있었습니다.
+        </p>
+        <p>새로운 세계를 맞이할 준비가 되셨나요?</p>
+        <p>위대한 시작을 외쳐보겠습니다.</p>
+      </div>
+      <div className="flex justify-center gap-4">
+        <WordInput />
+        <WordInput />
+        <WordInput />
+        <WordInput />
+      </div>
+    </div>
+  );
 }
---
+++ components/start/WordInput.tsx
@@ -0,0 +1,3 @@
+export default function WordInput() {
+  return <input className="size-16 rounded-md bg-slate-200 text-black" />;
+}
